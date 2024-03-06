d19db66a020c3d67b206e897e480172385ee5f49
Giselle Hoekveld <gisellehoekveld@gmail.com>
2024-03-06 08:22:11 -0300

    Initial commit

---
+++ build/js/tokens.js
@@ -0,0 +1,34 @@
+/**
+ * Do not edit directly
+ * Generated on Wed, 06 Mar 2024 11:19:17 GMT
+ */
+
+module.exports = {
+  "ColorWhite": "#ffffff",
+  "ColorDivider": "#e0e0e0",
+  "ColorBlack": "#000000",
+  "ColorBrandMain": "#8257e5",
+  "ColorBrandLight": "#996dff",
+  "ColorPriorityHigh": "#ff6060",
+  "ColorPriorityMedium": "#ffa500",
+  "ColorPriorityLow": "#5ca9d4",
+  "ColorPriorityRoutine": "#99beba",
+  "ColorPriorityEvent": "#996dff",
+  "ColorGray50": "#fafafa",
+  "ColorGray100": "#f5f5f5",
+  "ColorGray200": "#f0f0f0",
+  "ColorGray300": "#e2e2e2",
+  "ColorGray400": "#d3d3d3",
+  "ColorGray500": "#c4c4c4",
+  "ColorGray600": "#aaaaaa",
+  "ColorGray700": "#8b8b8b",
+  "ColorGray800": "#666666",
+  "ColorGray900": "#484848",
+  "ColorTagPrecept": "#faab34",
+  "ColorTagHome": "#5ca9d4",
+  "ColorTagReading": "#996dff",
+  "ColorTagFun": "#fd6cdd",
+  "ColorTagWork": "#9d530f",
+  "ColorTagSchool": "#49ac59",
+  "ColorTagCourses": "#735cff"
+};
